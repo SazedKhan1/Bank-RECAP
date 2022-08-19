5d8d27ed86ebc4daf8357e84a4a856796c5fa16f
Sazed_Khan <mamthedark233@gmail.com>
2022-08-19 22:50:23 +0600

    dhur bal

--- js/deposit.js
+++ js/deposit.js
@@ -1,36 +1,37 @@
-// step 1 add evenlistner in button
-
-document.getElementById("btn-deposit").addEventListener("click", function () {
-    // step 2 deposit amount ta input field theke nibo 
-    const depositField = document.getElementById("deposit-field");
+/* 1. add event listener to the deposit button
+2. get deposit amount from the deposit input field
+2-5. Convert string deposit amount to a number type
+3. clear the deposit input field after getting the value
+4. get the previous deposit total 
+5. calculate new deposit total and set the value to the deposit total
+6. get current balance total
+7. calculate the new balance and set it to the balance total element
+*/
+// step-1
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    // step-2
+    const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // step-3:
     depositField.value = '';
 
-    if (isNaN(newDepositAmount)) {
-        alert("Please provite an amount ")
-        return;
-    }
+    // step-4: 
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
+    // step-5:
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
 
+    // step-6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
 
-    // step 3 deposit total amount nibo and parseFloat korbo
-    const previousDepositTotal = document.getElementById("deposit-total");
-    const previousDepositAmountString = previousDepositTotal.innerText;
-    const previousDepositAmount = parseFloat(previousDepositAmountString);
-
-    // step 4 add the new deposit amount and previous deposit amount
-    const currentDepositAmount = newDepositAmount + previousDepositAmount;
-    previousDepositTotal.innerText = currentDepositAmount;
-
-    // step 5 deposit  amount add on total balance
-    const previousTotalAmount = document.getElementById("total-balance");
-    const previousTotalAmountString = previousTotalAmount.innerText;
-    const previousTotalAmountNumber = parseFloat(previousTotalAmountString);
-
-    // step 6 sum of balance and deposit 
-    const currentTotalAmount = previousTotalAmountNumber + newDepositAmount;
-    previousTotalAmount.innerText = currentTotalAmount;
-
-
+    // step-7:
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,34 +1,38 @@
+/* 
+1. add event handler with the withdraw button
+2. get the withdraw amount
+3. clear the withdraw input field
+4. get previous withdraw total
+5. calculate total Withdraw amount and set it to the withdraw total element
+6. get previous balance
+7. calculate new balance and set it to the balance total element
+*/
 
-document.getElementById("btn-withdraw").addEventListener("click", function () {
+// step-1
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // step-2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
-    const withdrawField = document.getElementById("withdraw-field");
-    const withdrawAmountString = withdrawField.value;
-    const withdrawAmount = parseFloat(withdrawAmountString);
+    // step-3
     withdrawField.value = '';
 
-    if (isNaN(withdrawAmount)) {
-        alert("Please provite an amount ")
-        return;
-    }
+    // step-4
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const previousWithdrawTotal = document.getElementById("withdraw-total");
-    const previousWithdrawTotalString = previousWithdrawTotal.innerText;
-    const withdrawTotal = parseFloat(previousWithdrawTotalString);
+    // step-5
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = newWithdrawTotal;
 
-    if (withdrawAmount > withdrawTotal) {
-        alert("Tor baper eto tk nai")
-        return;
-    }
-    const curentWithdrawAmount = withdrawAmount + withdrawTotal;
-    previousWithdrawTotal.innerText = curentWithdrawAmount;
+    // step-6
+    const balanceElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-
-    const previousTotalAmount = document.getElementById("total-balance");
-    const previousTotalAmountString = previousTotalAmount.innerText;
-    const previousTotalAmountNumber = parseFloat(previousTotalAmountString);
-
-    const currentTotalAmount = previousTotalAmountNumber - curentWithdrawAmount;
-    previousTotalAmount.innerText = currentTotalAmount;
-
-
+    // step-7
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceElement.innerText = newBalanceTotal;
 })
